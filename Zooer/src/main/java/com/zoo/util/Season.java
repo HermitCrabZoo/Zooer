0d518c830713e3ec1902468fe54c59e221754471
zoo <695863762@qq.com>
2017-09-03 17:14:36 +0800

    consummating...

--- Zooer/src/main/java/com/zoo/util/Season.java
+++ Zooer/src/main/java/com/zoo/util/Season.java
@@ -1,19 +1,24 @@
 package com.zoo.util;
 
 public enum Season {
-	spring("春",0,2,31),summer("夏",3,5,30),autumn("秋",6,8,30),winter("冬",9,11,31);
+	spring("春","Spring",0,2,31),summer("夏","Summer",3,5,30),autumn("秋","Autumn",6,8,30),winter("冬","Winter",9,11,31);
 	private final String name;
+	private final String ename;
 	private final int start;
 	private final int end;
 	private final int monthDay;
-	private Season(String name, int start, int end, int monthDay) {
+	private Season(String name,String ename, int start, int end, int monthDay) {
 		this.name = name;
+		this.ename = ename;
 		this.start = start;
 		this.end = end;
 		this.monthDay = monthDay;
 	}
 	public String getName() {
 		return name;
+	}
+	public String getEname() {
+		return ename;
 	}
 	public int getStart() {
 		return start;

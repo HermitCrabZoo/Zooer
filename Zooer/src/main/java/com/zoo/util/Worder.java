f9afbceb52e2ad5215143fcea301dde0da2758ff
zoo <695863762@qq.com>
2017-11-18 17:23:17 +0800

    make comment

--- Zooer/src/main/java/com/zoo/util/Worder.java
+++ Zooer/src/main/java/com/zoo/util/Worder.java
@@ -7,10 +7,19 @@ import org.apdplat.word.WordSegmenter;
 
 public final class Worder {
 	private Worder() {}
-	
+	/**
+	 * 对字符串分词，不保留中断词(的、是等。)
+	 * @param sentence
+	 * @return
+	 */
 	public static List<String> pureWords(String sentence) {
 		return WordSegmenter.seg(sentence).parallelStream().map(w->w.getText()).collect(Collectors.toList());
 	}
+	/**
+	 * 对字符串进行分词，保留重点词(的、是等)
+	 * @param sentence
+	 * @return
+	 */
 	public static List<String> intactWords(String sentence) {
 		return WordSegmenter.segWithStopWords(sentence).parallelStream().map(w->w.getText()).collect(Collectors.toList());
 	}

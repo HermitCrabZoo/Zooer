bc879b7f9dda6a09240342d92db247b8ca2e6a06
ZOOM <ZOOM@ZOOM>
2017-11-20 10:37:13 +0800

    optimized for the Packer.java!!

--- Zooer/src/main/java/com/zoo/util/Packer.java
+++ Zooer/src/main/java/com/zoo/util/Packer.java
@@ -24,22 +24,52 @@ public final class Packer {
 	private Packer() {}
 	
 	private static final int BUFFER = 4096;
+
 	
 	/**
 	 * 将file解压到文件夹dock里
-	 * @param file 不能为null，必须为可读的zip压缩包
+	 * @param zipPath
+	 * @param dock
+	 * @return 
+	 * @throws IOException
+	 * @see {@link #unzip(Path, Path, Charset)}
+	 */
+	public static Path unzip(Path zipPath,Path dock) throws IOException {
+		return unzip(zipPath, dock, null);
+	}
+	
+	/**
+	 * 将file解压到文件夹dock里，自动识别zip文件的字符集
+	 * @param zipPath
+	 * @param dock
+	 * @return
+	 * @throws IOException
+	 * @see {@link #unzip(Path, Path, Charset)}
+	 */
+	public static Path unzipAutoCharset(Path zipPath,Path dock) throws IOException {
+		return unzip(zipPath, dock, Charsetor.discern(zipPath, Charset.defaultCharset()));
+	}
+	
+	/**
+	 * 将file解压到文件夹dock里，指定字符集
+	 * @param zipPath 不能为null，必须为可读的zip压缩包
 	 * @param dock 不能为null，必须为可写的目录
+	 * @param charset 字符集，可为null
 	 * @return 返回输出目录(dock对象)
 	 * @throws IOException
 	 */
-	public static Path unzip(Path file,Path dock) throws IOException {
-		if (Filer.isReadableFile(file) && !Filer.isFile(dock)) {
+	public static Path unzip(Path zipPath,Path dock,Charset charset) throws IOException {
+		if (Filer.isReadableFile(zipPath) && !Filer.isFile(dock)) {
 			Filer.createDirIfNotExists(dock);//创建输出目录
 			String dest=dock.toString();
 			//识别压缩包的编码，识别不了则当UTF-8来处理
-			Charset charset=Charsetor.discern(file,Charset.defaultCharset());
 			//读取zip文件
-			ZipFile zFile=new ZipFile(file.toFile(), ZipFile.OPEN_READ, charset);
+			ZipFile zFile=null;
+			if(charset!=null) {
+				zFile=new ZipFile(zipPath.toFile(), ZipFile.OPEN_READ, charset);//设定字符集
+			}else {
+				zFile=new ZipFile(zipPath.toFile(), ZipFile.OPEN_READ);
+			}
 			Enumeration<? extends ZipEntry> zEntries=zFile.entries();
 			//遍历zip包里面的文件并输出
 			while (zEntries.hasMoreElements()) {
@@ -57,11 +87,12 @@ public final class Packer {
 							OutputStream os = new FileOutputStream(one.toFile());//创建解压后的文件 
 							BufferedOutputStream bos = new BufferedOutputStream(os);//带缓的写出流 
 					) {
+						int count;
 						byte [] b = new byte[BUFFER];//字节数组，每次读取4096个字节  
 						//循环读取压缩文件的值  
-						while(cos.read(b)!=-1)  
+						while((count=cos.read(b))!=-1)  
 						{  
-							bos.write(b);//写入到新文件  
+							bos.write(b,0,count);//写入到新文件  
 						}  
 					} catch (Exception e) {
 						e.printStackTrace();
@@ -76,18 +107,36 @@ public final class Packer {
 	
 	/**
 	 * 将dock压缩到名为zipPath的zip格式文件中。
+	 * @param dock
+	 * @param zipPath
+	 * @return
+	 * @throws IOException
+	 * @see {@link #zip(Path, Path, Charset)}
+	 */
+	public static Path zip(Path dock,Path zipPath) throws IOException {
+		return zip(dock, zipPath, null);
+	}
+	
+	/**
+	 * 将dock压缩到名为zipPath的zip格式文件中，指定压缩的字符集。
 	 * @param dock 需要被压缩的文件或文件夹，需要有可读权限。
 	 * @param zipPath 压缩后保存的zip文件，不能是已存在的目录，不能在dock以及dock的子目录下。
+	 * @param charset 压缩为指定的字符集
 	 * @return
 	 * @throws IOException
 	 */
-	public static Path zip(Path dock,Path zipPath) throws IOException {
+	public static Path zip(Path dock,Path zipPath,Charset charset) throws IOException {
 		if(Filer.isReadable(dock) && !Filer.isDir(zipPath) && !zipPath.startsWith(dock)) {
+			ZipOutputStream zos =null;
 			try(FileOutputStream fos = new FileOutputStream(zipPath.toFile());
 				CheckedOutputStream cos=new CheckedOutputStream(fos, new CRC32());
 				BufferedOutputStream bos = new BufferedOutputStream(fos);//用带缓冲的输出流包装
-				ZipOutputStream zos = new ZipOutputStream(bos);
 			) {
+				if (charset!=null) {
+					zos=new ZipOutputStream(bos,charset);//设定字符集
+				}else {
+					zos=new ZipOutputStream(bos);
+				}
 				byte [] b = new byte[BUFFER];
 				String parent=dock.getParent().toAbsolutePath().toString()+Platform.slash();
 				for(Path p:Filer.paths(dock, null)) {
@@ -110,6 +159,8 @@ public final class Packer {
 				};
 			} catch (IOException e) {
 				throw e;
+			}finally {
+				zos.close();
 			}
 		}
 		return dock;

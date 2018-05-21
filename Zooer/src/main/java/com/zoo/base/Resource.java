0defc0fbdbdbbb52a0a707e19257d4ffcc84c686
zoo <695863762@qq.com>
2018-05-21 22:39:20 +0800

    optimize...

--- Zooer/src/main/java/com/zoo/base/Resource.java
+++ Zooer/src/main/java/com/zoo/base/Resource.java
@@ -1,10 +1,13 @@
-package com.zoo.mix;
+package com.zoo.base;
 
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.swing.ImageIcon;
+
+import com.zoo.mix.Filer;
+import com.zoo.mix.Pather;
 
 /**
  * 获取项目资源类
@@ -14,6 +17,39 @@ import javax.swing.ImageIcon;
 public final class Resource
 {
 	private Resource(){}
+	
+	private static final String CP=getCp();
+	
+	private static String getCp(){
+        try{
+            URL url = Resource.class.getProtectionDomain().getCodeSource().getLocation();
+            Path path=Paths.get(url.toURI());
+            if (Filer.isFile(path)) {
+                path=path.getParent();
+            }
+            return path.toString();
+        } catch (Exception e){}
+        return "";
+    }
+	
+	/**
+	 * 获取类路径,若获取失败则返回空字符串
+	 * @return
+	 */
+	public static String classPath() {
+		return CP;
+	}
+	
+	/**
+	 * 获取classpath下的文件或目录
+	 * @param path 相对于classpath下的路径
+	 * @return
+	 */
+	public static Path fileOnClassPath(String path) {
+		return Paths.get(Pather.join(CP,path));
+	}
+	
+	
     /**
 	 * 传入图片名
 	 * @param imageName
@@ -41,24 +77,4 @@ public final class Resource
     	return icon;
 	}
     
-    /**
-     * 获取项目的bin目录或jar文件所在目录下的名为fileName的目录或文件，若获取失败则返回null。
-     * @param fileName
-     * @return
-     */
-    public static Path bins(String fileName){
-    	URL url = Resource.class.getProtectionDomain().getCodeSource().getLocation();
-    	Path path=null;
-        try{
-        	path=Paths.get(url.toURI());
-        	if (Filer.isFile(path)) {
-				path=path.getParent();
-			}else {
-				path=Paths.get(path.toString(), fileName);
-			}
-        } catch (Exception e){  
-            e.printStackTrace();
-        }  
-		return path;
-    }
 }

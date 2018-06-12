fa5d3e9fef9f2b084ff5e9742261fc00d01576a6
zoo <695863762@qq.com>
2018-06-12 12:36:55 +0800

    optimize...

--- Zooer/src/main/java/com/zoo/bean/Processor.java
+++ Zooer/src/main/java/com/zoo/bean/Processor.java
@@ -1,6 +1,8 @@
 package com.zoo.bean;
 
-import java.time.temporal.TemporalAccessor;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.Optional;
 
@@ -11,11 +13,40 @@ import com.zoo.mix.Dater;
 public interface Processor<T> {
 	T process(T property);
 
-	Processor<?> DATE_PROCESSOR = obj -> Optional.ofNullable(obj).map(o -> {
-		if (TemporalAccessor.class.isAssignableFrom(o.getClass())) {
-			return Dater.format((TemporalAccessor) o, Dater.ALL_FORMAT);
+	/**
+	 * 日期 时间转换处理器处理器
+	 */
+	Processor<?> DATE_TIME_PROCESSOR = obj -> Optional.ofNullable(obj).map(o -> {
+		if (LocalDateTime.class.isAssignableFrom(o.getClass())) {
+			return Dater.format((LocalDateTime) o, Dater.ALL_FORMAT);
 		} else if (Date.class.isAssignableFrom(o.getClass())) {
 			return Dater.format((Date) o, Dater.ALL_FORMAT);
+		}
+		return o;
+	}).orElse(Strs.empty());
+	
+	
+	/**
+	 * 日期转换处理器
+	 */
+	Processor<?> DATE_PROCESSOR = obj -> Optional.ofNullable(obj).map(o -> {
+		if (LocalDate.class.isAssignableFrom(o.getClass())) {
+			return Dater.format((LocalDate) o, Dater.YMD_FORMAT);
+		} else if (Date.class.isAssignableFrom(o.getClass())) {
+			return Dater.format((Date) o, Dater.YMD_FORMAT);
+		}
+		return o;
+	}).orElse(Strs.empty());
+	
+	
+	/**
+	 * 时间转换处理器
+	 */
+	Processor<?> TIME_PROCESSOR = obj -> Optional.ofNullable(obj).map(o -> {
+		if (LocalTime.class.isAssignableFrom(o.getClass())) {
+			return Dater.format((LocalTime) o, Dater.HMS_FORMAT);
+		} else if (Date.class.isAssignableFrom(o.getClass())) {
+			return Dater.format((Date) o, Dater.HMS_FORMAT);
 		}
 		return o;
 	}).orElse(Strs.empty());
